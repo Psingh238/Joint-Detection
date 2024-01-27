9d949de99aec1aba9e0507ae8e416ddc58023aa6
gord47 <97202987+gord47@users.noreply.github.com>
2024-01-27 10:22:09 -0800

    Mediapipe class implementation

---
+++ RealSensePython/FigurePoseDetect.py
@@ -0,0 +1,35 @@
+import time
+import numpy as np
+import cv2
+import math
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+class FigurePoseDetect:
+    def __init__(self):
+        model_path = 'pose_landmarker_full.task'
+
+        # Configure MediaPipe settings
+        BaseOptions = python.BaseOptions
+        PoseLandmarker = vision.PoseLandmarker
+        PoseLandmarkerOptions = vision.PoseLandmarkerOptions
+        self.PoseLandmarkerResult = vision.PoseLandmarkerResult
+        VisionRunningMode = vision.RunningMode
+        self.options = PoseLandmarkerOptions(
+            base_options=BaseOptions(model_asset_path=model_path),
+            running_mode=VisionRunningMode.LIVE_STREAM, result_callback=self.print_result
+            )
+
+    def print_result(self, result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+        landmarks = result.pose_landmarks
+    
+        # Ensure landmarks were actually returned or not
+        # This ensures list indexing is successful
+        if len(landmarks) != 0:
+            # Print out normalized landmarks for the nose
+            print('The result is {}'.format(landmarks[0][0]))
+
+
+
+
